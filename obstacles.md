1fefe04556610eacdf6b071b4ba7aac3b74bf82f
ralph <some@email.com>
2022-02-28 13:55:56 +0100

    Use html template and slots for cards
    Additionally some css and new line fixes

--- obstacles.md
+++ obstacles.md
@@ -1,2 +1,4 @@
 * Figuring out the term IIFE for a construct seen
   https://developer.mozilla.org/en-US/docs/Glossary/IIFE
+
+* html template/slot and getting bootstrap into shadow dom
--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 function loadFormPage(event) {
     let id = event.currentTarget.id;
-    let lang = "";
+    let lang;
 
     switch (id) {
         case "card-en":
@@ -9,13 +9,30 @@ function loadFormPage(event) {
         case "card-es":
             lang = "Spanish";
             break;
+        case "card-new":
+            lang = "Some language";
+            break;
         default:
             lang = "a bug occurred";
     }
-    
+
     sessionStorage.setItem("language", lang);
     window.location.assign("course.html");
 }
 
-document.getElementById("card-en").addEventListener("click", loadFormPage);
-document.getElementById("card-es").addEventListener("click", loadFormPage);
+customElements.define('course-card',
+    class extends HTMLElement {
+        constructor() {
+            super();
+            let template = document.getElementById('course-card');
+            let templateContent = template.content;
+
+            const shadowRoot = this.attachShadow({ mode: 'open' });
+            shadowRoot.appendChild(templateContent.cloneNode(true));
+        }
+    }
+);
+
+Array.from(document.getElementsByTagName("course-card")).forEach((card) => {
+    card.addEventListener("click", loadFormPage);
+});
